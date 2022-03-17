9e2a1215b73d8b69b511f00acb43979ea26bfe76
liuxiong <lg970044@foxmail.com>
2022-03-17 16:22:19 +0800

    feat: 表格设计列编排整理

--- src/pages/system/Table/[code]/col.tsx
+++ src/pages/system/Table/[code]/col.tsx
@@ -212,7 +212,7 @@ const ColForm: React.FC = () => {
                     x-component="Input"
                     x-component-props={{
                       style: {
-                        width: 100,
+                        width: 98,
                       },
                     }}
                     required
@@ -228,7 +228,7 @@ const ColForm: React.FC = () => {
                     x-component="Input"
                     x-component-props={{
                       style: {
-                        width: 100,
+                        width: 88,
                       },
                     }}
                     required
@@ -291,46 +291,6 @@ const ColForm: React.FC = () => {
                 </SchemaField.Void>
                 <SchemaField.Void
                   x-component="ArrayTable.Column"
-                  x-component-props={{ title: '编辑必填' }}
-                >
-                  <SchemaField.Boolean
-                    name="required"
-                    x-decorator="FormItem"
-                    x-component="Switch"
-                  />
-                </SchemaField.Void>
-                <SchemaField.Void
-                  x-component="ArrayTable.Column"
-                  x-component-props={{ title: '编辑默认值' }}
-                >
-                  <SchemaField.String
-                    name="default_value"
-                    x-decorator="FormItem"
-                    x-component="Input"
-                    x-component-props={{
-                      style: {
-                        width: 80,
-                      },
-                    }}
-                  />
-                </SchemaField.Void>
-                <SchemaField.Void
-                  x-component="ArrayTable.Column"
-                  x-component-props={{ title: '占用格数' }}
-                >
-                  <SchemaField.Boolean
-                    name="col_size"
-                    x-decorator="FormItem"
-                    x-component="NumberPicker"
-                    x-component-props={{
-                      style: {
-                        width: 60,
-                      },
-                    }}
-                  />
-                </SchemaField.Void>
-                <SchemaField.Void
-                  x-component="ArrayTable.Column"
                   x-component-props={{ title: '类型' }}
                 >
                   <SchemaField.Markup
@@ -339,7 +299,7 @@ const ColForm: React.FC = () => {
                     x-component="Select"
                     x-component-props={{
                       style: {
-                        width: 100,
+                        width: 110,
                       },
                     }}
                   />
@@ -354,42 +314,66 @@ const ColForm: React.FC = () => {
                     x-component="Select"
                     x-component-props={{
                       style: {
-                        width: 100,
+                        width: 110,
                       },
                     }}
                   />
                 </SchemaField.Void>
                 <SchemaField.Void
                   x-component="ArrayTable.Column"
-                  x-component-props={{ title: '表头筛选' }}
+                  x-component-props={{ title: '扩展' }}
                 >
-                  <SchemaField.Boolean name="filters" x-decorator="FormItem" x-component="Switch" />
-                </SchemaField.Void>
-                <SchemaField.Void
-                  x-component="ArrayTable.Column"
-                  x-component-props={{ title: '表头排序' }}
-                >
-                  <SchemaField.Boolean name="sorter" x-decorator="FormItem" x-component="Switch" />
-                </SchemaField.Void>
-                <SchemaField.Void
-                  x-component="ArrayTable.Column"
-                  x-component-props={{ title: '自动缩略' }}
-                >
-                  <SchemaField.Boolean
-                    name="ellipsis"
-                    x-decorator="FormItem"
-                    x-component="Switch"
-                  />
-                </SchemaField.Void>
-                <SchemaField.Void
-                  x-component="ArrayTable.Column"
-                  x-component-props={{ title: '支持复制' }}
-                >
-                  <SchemaField.Boolean
-                    name="copyable"
-                    x-decorator="FormItem"
-                    x-component="Switch"
-                  />
+                  <SchemaField.Void x-component="Editable.Popover" name="void_table" title="列表">
+                    <SchemaField.String
+                      name="col_size"
+                      title="占用格数"
+                      x-decorator="FormItem"
+                      x-component="NumberPicker"
+                      x-component-props={{
+                        style: {
+                          width: 60,
+                        },
+                      }}
+                    />
+                    <SchemaField.Boolean
+                      name="filters"
+                      title="表头筛选"
+                      x-decorator="FormItem"
+                      x-component="Switch"
+                    />
+                    <SchemaField.Boolean
+                      name="sorter"
+                      title="表头排序"
+                      x-decorator="FormItem"
+                      x-component="Switch"
+                    />
+                    <SchemaField.Boolean
+                      name="ellipsis"
+                      title="自动缩略"
+                      x-decorator="FormItem"
+                      x-component="Switch"
+                    />
+                    <SchemaField.Boolean
+                      name="copyable"
+                      title="支持复制"
+                      x-decorator="FormItem"
+                      x-component="Switch"
+                    />
+                  </SchemaField.Void>
+                  <SchemaField.Void x-component="Editable.Popover" name="void_form" title="编辑">
+                    <SchemaField.String
+                      name="default_value"
+                      title="默认值"
+                      x-decorator="FormItem"
+                      x-component="Input"
+                    />
+                    <SchemaField.Boolean
+                      name="required"
+                      title="必填"
+                      x-decorator="FormItem"
+                      x-component="Switch"
+                    />
+                  </SchemaField.Void>
                 </SchemaField.Void>
                 <SchemaField.Void
                   x-component="ArrayTable.Column"
