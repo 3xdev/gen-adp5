4cc9088ae7785a01eac86ca32bf3a2d27f94cc4e
liux <lg970044@foxmail.com>
2023-03-15 16:31:12 +0800

    fix: 管理员角色

--- src/pages/system/Admin/index.tsx
+++ src/pages/system/Admin/index.tsx
@@ -119,15 +119,8 @@ const AdminTable: React.FC = () => {
       title: '角色',
       dataIndex: 'roles',
       valueEnum: { ...roles },
-      search: false,
-    },
-    {
-      title: '角色',
-      dataIndex: 'roles[]',
-      valueEnum: { ...roles },
       valueType: 'select',
       fieldProps: { mode: 'multiple' },
-      hideInTable: true,
     },
     {
       title: '密码',
